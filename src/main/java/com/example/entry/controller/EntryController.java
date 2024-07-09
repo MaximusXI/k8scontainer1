9a222c3e1063c52f34de5866a16dd8ec195915aa
MaximusXI <harshilmakwana34@gmail.com>
2024-07-09 12:23:50 -0300

    updated the log in EntryController

--- src/main/java/com/example/entry/controller/EntryController.java
+++ src/main/java/com/example/entry/controller/EntryController.java
@@ -33,7 +33,7 @@ public class EntryController {
     @PostMapping("store-file")
     public ResponseEntity<Map<String,String>> storeFileData(@RequestBody FileRequest request) throws Exception {
         System.out.println(secondServiceUrl);
-        log.info("A simple log 3");
+        log.info("A simple log 5");
         log.info("Inside the EntryController in the store-file endpoint");
         log.info("The second service url is: {}", secondServiceUrl);
         Map<String,String> result = checkUtil.storeFile(request,fileLocation);
